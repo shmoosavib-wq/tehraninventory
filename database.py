625b8a98b56ed6279d8ffdb93c8fcf5f56a3a22c
Hamid Moosavi <shmoosavib@gmail.com>
2026-08-30 17:50:58 +0330

    Update_database_path_for_railway_volume

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tehran_inventory.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tehran_inventory.db")
 # SQLite needs check_same_thread=False for FastAPI async
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
